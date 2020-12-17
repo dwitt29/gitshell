bd7156eb7bf4b621d80d3138b87585557412acbf
Dave <dwitt29@verizon.net>
2020-12-17 12:11:16 -0500

    helpful updates

--- puppet_help.bash
+++ puppet_help.bash
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# show factpath
+puppet agent --configprint factpath
+
 # install prebuild puppet module
 puppet module install puppetlabs-ntp --target-dir .
 
@@ -12,3 +15,8 @@ puppet config print modulepath
 # cd into <module name> & then run to create a new class
 /usr/local/bin/pdk new class ntp::install
 
+# fix for git ignore empty directories
+find . -type d -empty -print -exec touch {}/.gitkeep \;
+
+# check values of hiera/yaml variables
+puppet lookup etc::etc_shadow_path
