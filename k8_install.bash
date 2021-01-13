14ffd37309bc6b6755e52a160fab1af45d4f9664
Dave <dwitt29@verizon.net>
2021-01-12 21:04:07 -0500

    add info for docker install

--- k8_install.bash
+++ k8_install.bash
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# from https://phoenixnap.com/kb/how-to-install-docker-centos-7
+
+yum check-update
+yum install -y yum-utils device-mapper-persistent-data lvm2
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+yum install docker
+systemctl start docker
+systemctl enable docker
+
 # from https://phoenixnap.com/kb/how-to-install-kubernetes-on-centos
 
 # step 1
