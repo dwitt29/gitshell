16081abb72a60b75e4ff008c2ecc17516eaeb2a1
Dave <dwitt29@verizon.net>
2021-02-01 21:44:04 -0500

    fix info regarding docker install

--- k8_install.bash
+++ k8_install.bash
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 # from https://phoenixnap.com/kb/how-to-install-docker-centos-7
+# https://docs.docker.com/engine/install/centos/
 
 yum check-update
 yum install -y yum-utils device-mapper-persistent-data lvm2
 yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-yum install docker
+yum install docker-ce docker-ce-cli containerd.io
 systemctl start docker
 systemctl enable docker
 
